8df05160b8278f34a9d4d7b416d7b7f43b848ebd
kevinkorza <kevingkorza@gmail.com>
2022-12-08 19:03:52 -0500

    back end server.js construction/build

---
+++ server/server.js
@@ -0,0 +1,22 @@
+//Dependencies
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3008;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
