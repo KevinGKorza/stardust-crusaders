79486e5cfbcb8e688d66ce69bc2f59604e62c774
Ken Clark <kwc2894@rit.edu>
2022-12-12 19:17:36 -0500

    queries TEMPORARY IN CASE I MISS ANYTHING

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import gql from "graphql-tag";
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      gameCount
+      savedGames {
+        gameId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
