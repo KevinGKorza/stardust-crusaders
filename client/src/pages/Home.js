e4b0076fa83768775c4ce400f05a2dc1ec7d0a7a
kevinkorza <kevingkorza@gmail.com>
2022-12-17 17:21:58 -0500

    home page images update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,26 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h1>GameStart_Home</h1>
+            <h1>Your Games</h1>
+            <img src={require('../../src/Images/super-mario-bros.webp')} height={500} width={500} />
+            <br>
+            </br>
+            <br>
+            </br>
+            <img src={require('../../src/Images/Super_Mario_Bros._box.png')} height={500} width={500} />
+            <br>
+            </br>
+            <br>
+            </br>
+            <img src={require('../../src/Images/jp.webp')} height={500} width={500} />
+            <br>
+            </br>
+            <br>
+            </br>
+            <img src={require('../../src/Images/154-1542965_super-mario-bros-nes-box-art-hd-png.png')} height={500} width={500} />
+
+
+
         </div>
     );
 };
